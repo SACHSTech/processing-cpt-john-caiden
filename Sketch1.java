1be21afd228665f6b14a65822e33952af26b5bb8
CaidenHong <caiden.hong23@ycdsbk12.ca>
2022-05-31 09:00:06 -0400

    Sonic SpriteSheet

--- Sketch1.java
+++ Sketch1.java
@@ -1,36 +1,58 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch1 extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
+// Importing Images
+PImage sonic_spritesheet;
+PImage sonic_runningsheet;
+PImage[] sonic_frames;
+
+// Declaring Variables
+int intSonic_frames = 8;
+int intSonic_frameWidth = 40;
+int intSonic_frameHeight = 40;
+int intSonicX = 20;
+int intSonicY = 80;
+
+
+
+public void settings() {
+// put your size call here
+  size(400, 400);
+
+}
+
+
+public void setup() {
+  background(210, 255, 173);
+
+  // Load Sonic Spritesheet
+  sonic_spritesheet = loadImage("Sonicsheet.png");
+  sonic_runningsheet = sonic_spritesheet.get(2,267, intSonic_frameWidth*intSonic_frames, intSonic_frameHeight );
+
+  // load the sonic frames from the spritesheet
+  sonic_frames = new PImage[intSonic_frames];
+  for(int frameNum = 0; frameNum < intSonic_frames; frameNum++ ){
+    System.out.println("load frames");
+    sonic_frames[frameNum] = sonic_runningsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight );
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+}
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+public void draw() {
+  // Background
+  background(0, 0, 0);
+
+  // Draw Sonic to Screen
+  image(sonic_frames[(frameCount/3)%intSonic_frames], intSonicX, intSonicY);
+  intSonicX += 2;
+
+  if(intSonicX > width){
+    intSonicX = 0 - intSonic_frameWidth;
+
   }
   
-  // define other methods down here.
+}
 }
