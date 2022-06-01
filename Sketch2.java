c741f210e1cde764897d20e7d319c54543790277
JohnDuarte23 <John.Duarte23@ycdsbk12.ca>
2022-06-01 08:39:28 -0400

    run animation

--- Sketch2.java
+++ Sketch2.java
@@ -1,7 +1,16 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch2 extends PApplet {
 	
+  PImage sonic_spritesheet;
+  PImage sonic_runningsheet;
+  PImage[] sonic_frames;
+  int intSonic_frames = 8;
+  int intSonic_frameWidth = 40;
+  int intSonic_frameHeight = 40;
+  int intSonicX = 20;
+  int intSonicY = 80;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -16,20 +25,32 @@ public class Sketch2 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+
+    background(0);
+
+    sonic_spritesheet = loadImage("Sonicsheet.png");
+    sonic_runningsheet = sonic_spritesheet.get(2,267, intSonic_frameWidth*intSonic_frames, intSonic_frameHeight );
+
+    sonic_frames = new PImage[intSonic_frames];
+    for(int frameNum = 0; frameNum < intSonic_frames; frameNum++ ){
+      System.out.println("load frames");
+      sonic_frames[frameNum] = sonic_runningsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight );
+    }
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  image(sonic_frames[(frameCount/3)%intSonic_frames], intSonicX, intSonicY);
+    intSonicX += 2;
+
+    if(intSonicX > width){
+      intSonicX = 0 - intSonic_frameWidth;
+    }
+    
+    
   }
   
   // define other methods down here.
