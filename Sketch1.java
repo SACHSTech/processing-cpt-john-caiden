63a218d247fb9e2537e2ff53048d66a767b8f6d6
JohnDuarte23 <JohnDuarte23@ycdsbk12.ca>
2022-06-09 22:39:58 -0400

    sonic standing still and running left animations

--- Sketch1.java
+++ Sketch1.java
@@ -8,17 +8,20 @@ PImage sonic_spritesheet;
 
 PImage sonic_rightsheet;
 PImage sonic_leftsheet;
+PImage sonic_stillsheet;
 
 PImage[] sonic_right;
 PImage[] sonic_left;
+PImage[] sonic_still;
 
 // Declaring Variables
 int intSonic_right = 8;
 int intSonic_left = 8;
+int intSonic_still = 3;
 int intSonic_frameWidth = 40;
 int intSonic_frameHeight = 40;
-int intSonicX = 20;
-int intSonicY = 80;
+int intSonicX;
+int intSonicY;
 boolean dPressed = false;
 boolean aPressed = false;
 
@@ -31,23 +34,34 @@ public void settings() {
 public void setup() {
   background(210, 255, 173);
 
-  // Load Sonic Runnning Right Spritesheet
-  sonic_spritesheet = loadImage("Sonicsheet.png");
-  sonic_rightsheet = sonic_spritesheet.get(2,267, intSonic_frameWidth*intSonic_right, intSonic_frameHeight );
+  // load sonic runnning right spritesheet
+  sonic_spritesheet = loadImage("Sonicsheet right.png");
+  sonic_rightsheet = sonic_spritesheet.get(2,267, intSonic_frameWidth*intSonic_right, intSonic_frameHeight);
+
+  // load sonic standing still spritesheet
+  sonic_spritesheet = loadImage("Sonicsheet right.png");
+  sonic_stillsheet = sonic_spritesheet.get(222, 816, intSonic_frameWidth*intSonic_right, intSonic_frameHeight);
 
   // load sonic running left spritesheet
-  sonic_spritesheet = loadImage("Sonicsheet right.png");
-  sonic_leftsheet = sonic_spritesheet.get();
+  sonic_spritesheet = loadImage("Sonicsheet left.png");
+  sonic_leftsheet = sonic_spritesheet.get(244, 267, intSonic_frameWidth*intSonic_left, intSonic_frameHeight);
 
-  // load the sonic Running Right from the spritesheet
+  // load the sonic running right from the spritesheet
   sonic_right = new PImage[intSonic_right];
   for(int frameNum = 0; frameNum < intSonic_right; frameNum++ ){
-    sonic_right[frameNum] = sonic_rightsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight );
+    sonic_right[frameNum] = sonic_rightsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight);
   }
 
+  // load the sonic running left from the spritesheet
   sonic_left = new PImage[intSonic_left];
-  for(int frameNum = 0; frameNum < intSonic_right; frameNum++ ){
+  for(int frameNum = 0; frameNum < intSonic_left; frameNum++ ){
     sonic_left[frameNum] = sonic_leftsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight);
+  }
+
+  // load the sonic standing still from the spritesheet
+  sonic_still = new PImage[intSonic_still];
+  for(int frameNum = 0; frameNum < intSonic_still; frameNum++) {
+    sonic_still[frameNum] = sonic_stillsheet.get(intSonic_frameWidth*frameNum, 0, intSonic_frameWidth, intSonic_frameHeight);
   }
 
 }
@@ -65,15 +79,12 @@ public void draw() {
     intSonicX += 2;
     image(sonic_right[(frameCount/3)%intSonic_right], intSonicX, intSonicY);
    } 
-
-  if (aPressed) {
+  else if (aPressed) {
     intSonicX -= 2;
-    image(sonic_left[(frameCount/3)%intSonic_right], intSonicX, intSonicY);
+    image(sonic_left[(frameCount/3)%intSonic_left], intSonicX, intSonicY);
   }
-
-  if (intSonicX == 0) {
-    // sonic standing spritesheet
-
+  else {
+    image(sonic_still[(frameCount/10)%intSonic_still], intSonicX, intSonicY);
   }
    
  }
